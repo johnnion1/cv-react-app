b232c3c302d8a6a8f93ff63e12850367542fd663
johnnion1 <84983555+johnnion1@users.noreply.github.com>
2024-08-29 12:20:01 +0200

    add functions for custom input and button elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,25 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {}
+
+function CustomInput({ type, value, label, name, onChange }) {
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={(e, name) => onChange(e, name)}
+      />
+    </>
+  );
+}
+function CustomButton(text, type, handleClick) {
+  return (
+    <button type={type} onClick={handleClick}>
+      {text}
+    </button>
+  );
+}
 export default App;
