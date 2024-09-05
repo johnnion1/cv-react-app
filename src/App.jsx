5e2f62e8b61312c29ed3a9b43c19a20e747e4be3
johnnion1 <84983555+johnnion1@users.noreply.github.com>
2024-09-05 13:14:29 +0200

    add EducationalForm component, update PersonalForm
    Still needs an 'add' function to add entries
    to educational (and work) forms.
    Labels need to be adjusted and a date range put in
    for years. Not very dry because it's mostly a copy
    of PersonalForm.
    
    Update structure of the custom form components:
    Input fields can be placed better over the <p>
    elements that replace them when editActive=false.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./assets/styles/App.css";
 
 function App() {
   const [personal, setPersonal] = useState({
@@ -8,18 +8,35 @@ function App() {
     address: "",
     age: "",
   });
+  const [educational, setEducational] = useState({
+    school: "",
+    study: "",
+    year: "",
+  });
 
   return (
     <>
-      <p>Hyello</p> <h2>{personal.firstName + " " + personal.lastName}</h2>
-      <br />
-      <hr />
-      <PersonalForm personal={personal} setPersonal={(v) => setPersonal(v)} />
+      <div className="mainContent">
+        <p>Hyello</p> <h2>{personal.firstName + " " + personal.lastName}</h2>
+        <br />
+        <hr />
+        <PersonalForm
+          className="personalform"
+          personal={personal}
+          setPersonal={(v) => setPersonal(v)}
+        />
+        <hr />
+        <EducationalForm
+          className="educationalForm"
+          educational={educational}
+          setEducational={(v) => setEducational(v)}
+        />
+      </div>
     </>
   );
 }
 
-function PersonalForm({ personal, setPersonal }) {
+function PersonalForm({ formClassName, personal, setPersonal }) {
   const [editedPersonal, setEditedPersonal] = useState(personal);
   const [editedBool, setEditedBool] = useState(false); /* Use object */
   const [editActive, setEditActive] = useState(false);
@@ -45,103 +62,236 @@ function PersonalForm({ personal, setPersonal }) {
   }
   return (
     <>
-      <form action="" method="post">
-        {editActive ? (
-          <CustomInput
-            value={editedPersonal.firstName}
-            label="First name: "
-            name="firstName"
-            id="firstName"
-            onChange={(e) => handleChange(e, "firstName")}
-          />
-        ) : (
+      <div className="formDiv">
+        <h2>Personal Details</h2>
+        <form action="" method="post">
           <div className="infoLine">
-            <p>First name:</p>
-            <p> {editedPersonal.firstName}</p>
+            {editActive ? (
+              <CustomInput
+                className="slideInput"
+                value={editedPersonal.firstName}
+                label="First name: "
+                name="firstName"
+                id="firstName"
+                onChange={(e) => handleChange(e, "firstName")}
+              />
+            ) : (
+              <>
+                <p>First name:</p>
+                <p> {editedPersonal.firstName}</p>
+              </>
+            )}
           </div>
-        )}
-        <br />
-        {editActive ? (
-          <CustomInput
-            value={editedPersonal.lastName}
-            label="Last Name: "
-            name="lastName"
-            onChange={(e) => handleChange(e, "lastName")}
-          />
-        ) : (
+
           <div className="infoLine">
-            <p>Last name:</p>
-            <p> {editedPersonal.lastName}</p>
+            {editActive ? (
+              <CustomInput
+                value={editedPersonal.lastName}
+                label="Last name: "
+                name="lastName"
+                onChange={(e) => handleChange(e, "lastName")}
+              />
+            ) : (
+              <>
+                <p>Last name:</p>
+                <p>
+                  {" "}
+                  {editedPersonal.lastName == ""
+                    ? " "
+                    : editedPersonal.lastName}
+                </p>
+              </>
+            )}
           </div>
-        )}
-        <br />
-        {editActive ? (
-          <CustomInput
-            value={editedPersonal.address}
-            label="Address: "
-            name="address"
-            onChange={(e) => handleChange(e, "address")}
-          />
-        ) : (
+
           <div className="infoLine">
-            <p>Address: </p>
-            <p> {editedPersonal.address}</p>
+            {editActive ? (
+              <CustomInput
+                value={editedPersonal.address}
+                label="Address: "
+                name="address"
+                onChange={(e) => handleChange(e, "address")}
+              />
+            ) : (
+              <>
+                <p>Address: </p>
+                <p> {editedPersonal.address}</p>
+              </>
+            )}
           </div>
-        )}
-        <br />
-        {editActive ? (
-          <CustomInput
-            type="date"
-            value={editedPersonal.age}
-            label="Age: "
-            name="age"
-            onChange={(e) => handleChange(e, "age")}
-          />
-        ) : (
+
           <div className="infoLine">
-            <p>Age:</p>
-            <p> {editedPersonal.age}</p>
+            {editActive ? (
+              <CustomInput
+                type="date"
+                value={editedPersonal.age}
+                label="Age: "
+                name="age"
+                id="age"
+                onChange={(e) => handleChange(e, "age")}
+                range={["1900-01-01", "2010, 01-01"]}
+              />
+            ) : (
+              <>
+                <p>Age:</p>
+                <p> {editedPersonal.age}</p>
+              </>
+            )}
           </div>
-        )}
-        <CustomButton
-          isActive={editedBool === true ? true : false}
-          text="Submit Changes"
-          type="submit"
-          handleClick={handleSubmit}
-          hidden={editActive ? false : true}
-        />
-        <CustomButton
-          isActive={true}
-          text={editActive ? "Back" : "Edit"}
-          type="default"
-          handleClick={(e) => {
-            e.preventDefault();
-            editActive ? setEditActive(false) : setEditActive(true);
-          }}
-          hidden={false}
-        />
-      </form>
+          <div className="buttonDiv">
+            <CustomButton
+              isActive={editedBool === true ? true : false}
+              text="Submit Changes"
+              type="submit"
+              handleClick={handleSubmit}
+              hidden={editActive ? false : true}
+            />
+            <CustomButton
+              isActive={true}
+              text={editActive ? "Back" : "Edit"}
+              type="default"
+              handleClick={(e) => {
+                e.preventDefault();
+                editActive ? setEditActive(false) : setEditActive(true);
+              }}
+              hidden={false}
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+function EducationalForm({ formClassName, educational, setEducational }) {
+  const [editedEducational, setEditedEducational] = useState(educational);
+  const [editedBool, setEditedBool] = useState(false); /* Use object */
+  const [editActive, setEditActive] = useState(false);
+  /* Lift into parrent, use activeIndex 0,1,2 instead */ /*let edited = false; Either put in state or calculate if editedEducational is different from personal */
+
+  function handleChange(e, inputName) {
+    const newEducational = {
+      ...editedEducational,
+      [inputName]: e.target.value,
+    };
+    setEditedBool(true);
+    if (newEducational[inputName] == educational[inputName]) {
+      console.log("newEducational[inputName] == personal[inputName]");
+      setEditedBool(false);
+    }
+    setEditedEducational(newEducational);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (editedBool) {
+      setEducational(editedEducational);
+      setEditActive(false);
+      setEditedBool(false);
+      return;
+    }
+  }
+  return (
+    <>
+      <div className="formDiv">
+        <h2>Educational Details</h2>
+        <form action="" method="post">
+          <div className="infoLine">
+            {editActive ? (
+              <CustomInput
+                value={editedEducational.school}
+                label="School: "
+                name="school"
+                id="school"
+                onChange={(e) => handleChange(e, "school")}
+              />
+            ) : (
+              <>
+                <p>School:</p>
+                <p> {editedEducational.school}</p>
+              </>
+            )}
+          </div>
+
+          <div className="infoLine">
+            {editActive ? (
+              <CustomInput
+                value={editedEducational.study}
+                label="Study: "
+                name="study"
+                id="study"
+                onChange={(e) => handleChange(e, "study")}
+              />
+            ) : (
+              <>
+                <p>Study:</p>
+                <p> {editedEducational.study}</p>
+              </>
+            )}
+          </div>
+          <div className="infoLine">
+            {editActive ? (
+              <CustomInput
+                type="date"
+                value={editedEducational.year}
+                label="year: "
+                name="year"
+                onChange={(e) => handleChange(e, "year")}
+              />
+            ) : (
+              <>
+                <p>Year:</p>
+                <p> {editedEducational.year}</p>
+              </>
+            )}
+          </div>
+
+          <div className="buttonDiv">
+            <CustomButton
+              isActive={editedBool === true ? true : false}
+              text="Submit Changes"
+              type="submit"
+              handleClick={handleSubmit}
+              hidden={editActive ? false : true}
+            />
+            <CustomButton
+              isActive={true}
+              text={editActive ? "Back" : "Edit"}
+              type="default"
+              handleClick={(e) => {
+                e.preventDefault();
+                editActive ? setEditActive(false) : setEditActive(true);
+              }}
+              hidden={false}
+            />
+          </div>
+        </form>
+      </div>
     </>
   );
 }
 
-function CustomInput({ type, value, label, name, onChange }) {
+function CustomInput({ className, type, value, label, name, onChange, range }) {
   return (
     <>
       <label>{label}</label>
       <input
+        className={className}
+        placeholder="has"
         type={type}
         value={value}
         name={name}
         onChange={(e, name) => onChange(e, name)}
+        min={range ? range[0] : null}
+        max={range ? range[1] : null}
       />
+      {/* 
+      <span></span> */}
     </>
   );
 }
 function CustomButton({ text, type, handleClick, isActive, hidden }) {
   return (
     <button
-      type={type}
+      type={type ? type : "text"}
       onClick={handleClick}
       disabled={!isActive}
       hidden={hidden}
